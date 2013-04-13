844075a8b82be9adbf92decb6a8db4154db029f8
shiba_yu36 <shibayu36@gmail.com>
2013-04-13 19:49:32 +0900

    use coro for concurrency

--- lib/Cinnamon/Runner/Concurrent.pm
+++ lib/Cinnamon/Runner/Concurrent.pm
@@ -2,35 +2,27 @@ package Cinnamon::Runner::Concurrent;
 use strict;
 use warnings;
 
-use Parallel::ForkManager;
-
 use Cinnamon::Logger;
 use Cinnamon::Config;
 
+use Coro;
+use Coro::Select;
+
 sub start {
     my ($class, $hosts, $task, @args) = @_;
-
-    my $concurrency = scalar @$hosts;
-    my $pm = Parallel::ForkManager->new($concurrency);
+    my @coros;
     my $all_results = {};
 
-    # result handling
-    $pm->run_on_finish(sub {
-        my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $data) = @_;
-        my $host  = $data->{host};
-        my $error = $data->{error};
-        $all_results->{$host} = { error => $error };
-    });
-
     for my $host (@$hosts) {
-        my $pid = $pm->start and next;
+        my $coro = async {
+            my $result = $class->execute($host, $task, @args);
+            $all_results->{$host} = $result;
+        };
 
-        my $result = $class->execute($host, $task, @args);
-
-        $pm->finish(0, $result);
+        push @coros, $coro;
     }
 
-    $pm->wait_all_children;
+    $_->join for @coros;
 
     return $all_results;
 }
