8538d96d95fe8804b61d0fc04724011bff6044aa
shiba_yu36 <shibayu36@gmail.com>
2013-04-14 02:15:48 +0900

    max_concurrency setting

--- lib/Cinnamon/Runner/Concurrent.pm
+++ lib/Cinnamon/Runner/Concurrent.pm
@@ -10,19 +10,26 @@ use Coro::Select;
 
 sub start {
     my ($class, $hosts, $task, @args) = @_;
-    my @coros;
     my $all_results = {};
+    $hosts = [ @$hosts ];
 
-    for my $host (@$hosts) {
-        my $coro = async {
-            my $result = $class->execute($host, $task, @args);
-            $all_results->{$host} = $result;
-        };
+    my $task_name           = Cinnamon::Config::get('task');
+    my $concurrency_setting = Cinnamon::Config::get('max_concurrency') || {};
+    my $concurrency         = $concurrency_setting->{$task_name}
+        || scalar @$hosts;
 
-        push @coros, $coro;
+    while (my @target_hosts = splice @$hosts, 0, $concurrency) {
+        my @coros;
+        for my $host (@target_hosts) {
+            my $coro = async {
+                my $result = $class->execute($host, $task, @args);
+                $all_results->{$host} = $result;
+            };
+
+            push @coros, $coro;
+        }
+        $_->join for @coros;
     }
-
-    $_->join for @coros;
 
     return $all_results;
 }
