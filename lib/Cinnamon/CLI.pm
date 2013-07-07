13483c52b834de3042322ca31d79eddac09ef1d1
shiba_yu36 <shibayu36@gmail.com>
2013-07-07 15:11:38 +0900

    remove use Cinnamon

--- lib/Cinnamon/CLI.pm
+++ lib/Cinnamon/CLI.pm
@@ -3,18 +3,13 @@ use strict;
 use warnings;
 
 use Getopt::Long;
-use Cinnamon;
+use Cinnamon::Context;
 
 use constant { SUCCESS => 0, ERROR => 1 };
 
 sub new {
     my $class = shift;
     bless { }, $class;
-}
-
-sub cinnamon {
-    my $self = shift;
-    $self->{cinnamon} ||= Cinnamon->new;
 }
 
 sub run {
@@ -57,8 +52,9 @@ sub run {
 
     @tasks = (undef) if (@tasks == 0);
     my $error_occured = 0;
+    my $context = Cinnamon::Context->new;
     for my $task (@tasks) {
-        my ($success, $error) = $self->cinnamon->run(
+        my ($success, $error) = $context->run(
             $role,
             $task,
             config            => $self->{config},
