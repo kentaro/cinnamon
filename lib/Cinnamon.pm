9e28246f3213e3622b602b224bbe408072214c36
shiba_yu36 <shibayu36@gmail.com>
2013-07-07 15:08:09 +0900

    move Cinnamon.pm implements to Context.pm

--- lib/Cinnamon.pm
+++ lib/Cinnamon.pm
@@ -5,12 +5,7 @@ use 5.010_001;
 
 our $VERSION = '0.23';
 
-use YAML ();
-use Class::Load ();
-
-use Cinnamon::Config;
-use Cinnamon::Runner;
-use Cinnamon::Logger;
+use Cinnamon::Context;
 
 sub new {
     my $class = shift;
@@ -19,51 +14,8 @@ sub new {
 
 sub run {
     my ($self, $role, $task, %opts)  = @_;
-    my @args     = Cinnamon::Config::load $role, $task, %opts;
 
-    if ($opts{info}) {
-        log 'info', YAML::Dump(Cinnamon::Config::info);
-        return;
-    }
-
-    my $hosts    = Cinnamon::Config::get_role;
-    my $task_def = Cinnamon::Config::get_task;
-    my $runner   = Cinnamon::Config::get('runner_class') || 'Cinnamon::Runner';
-
-    unless (defined $hosts) {
-        log 'error', "undefined role : '$role'";
-        return;
-    }
-    unless (defined $task_def) {
-        log 'error', "undefined task : '$task'";
-        return;
-    }
-
-    Class::Load::load_class $runner;
-
-    my $result = $runner->start($hosts, $task_def);
-    my (@success, @error);
-
-    for my $key (keys %{$result || {}}) {
-        if ($result->{$key}->{error}) {
-            push @error, $key;
-        }
-        else {
-            push @success, $key;
-        }
-    }
-
-    log success => sprintf(
-        "\n========================\n[success]: %s",
-        (join(', ', @success) || ''),
-    );
-
-    log error => sprintf(
-        "[error]: %s",
-        (join(', ', @error)   || ''),
-    );
-
-    return (\@success, \@error);
+    return Cinnamon::Context->new->run($role, $task, %opts);
 }
 
 !!1;
---
+++ lib/Cinnamon/Context.pm
@@ -0,0 +1,66 @@
+package Cinnamon::Context;
+use strict;
+use warnings;
+
+use YAML ();
+use Class::Load ();
+
+use Cinnamon::Config;
+use Cinnamon::Runner;
+use Cinnamon::Logger;
+
+sub new {
+    my $class = shift;
+    bless { }, $class;
+}
+
+sub run {
+    my ($self, $role, $task, %opts)  = @_;
+    my @args     = Cinnamon::Config::load $role, $task, %opts;
+
+    if ($opts{info}) {
+        log 'info', YAML::Dump(Cinnamon::Config::info);
+        return;
+    }
+
+    my $hosts    = Cinnamon::Config::get_role;
+    my $task_def = Cinnamon::Config::get_task;
+    my $runner   = Cinnamon::Config::get('runner_class') || 'Cinnamon::Runner';
+
+    unless (defined $hosts) {
+        log 'error', "undefined role : '$role'";
+        return;
+    }
+    unless (defined $task_def) {
+        log 'error', "undefined task : '$task'";
+        return;
+    }
+
+    Class::Load::load_class $runner;
+
+    my $result = $runner->start($hosts, $task_def);
+    my (@success, @error);
+
+    for my $key (keys %{$result || {}}) {
+        if ($result->{$key}->{error}) {
+            push @error, $key;
+        }
+        else {
+            push @success, $key;
+        }
+    }
+
+    log success => sprintf(
+        "\n========================\n[success]: %s",
+        (join(', ', @success) || ''),
+    );
+
+    log error => sprintf(
+        "[error]: %s",
+        (join(', ', @error)   || ''),
+    );
+
+    return (\@success, \@error);
+}
+
+!!1;
